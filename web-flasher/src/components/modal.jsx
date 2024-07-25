fa548bbed4ef8e152f9af16ac7634179608dbe1c
TeaRex-coder <alexandre.ottawa@gmail.com>
2024-07-24 22:05:02 -0400

    update stripe

--- web-flasher/src/components/modal.jsx
+++ web-flasher/src/components/modal.jsx
@@ -20,7 +20,7 @@ const ModalComponent = () => {
               HardwareDDC
             </p>
             <p className="text-lg font-medium text-neutral-800 dark:text-neutral-50">
-              CA$65
+              CA$100
             </p>
             <p className="text-sm text-neutral-800 dark:text-neutral-50">
               Currently shipping to Canada & America. If you need another
@@ -51,7 +51,7 @@ const ModalComponent = () => {
               </a>
             </div>
             <a
-              href="https://buy.stripe.com"
+              href="https://buy.stripe.com/fZecP9gmlfJ04Pm8wx"
               target="_blank"
               rel="noopener noreferrer"
               className="install-button rounded-lg border-2 border-transparent px-4 py-2 text-base font-medium bg-neutral-950 text-neutral-50 hover:bg-cyan-900 hover:text-neutral-50 hover:border-cyan-600 dark:bg-neutral-50 dark:text-neutral-950 transition duration-300 ease-in-out cursor-pointer"
